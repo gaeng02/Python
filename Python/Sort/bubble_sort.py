acb4a08651ba2bca4d1980924c4778f32f30c3d0
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-03-05 00:48:49 +0900

    Update Bubble_Sort.py

--- Python/Sort/bubble_sort.py
+++ Python/Sort/bubble_sort.py
@@ -1,1 +1,18 @@
-def bubble_sort ()
+def bubble_sort (array: list) -> list : 
+
+    n = len(array)
+    
+    for i in range (n-1, 0, -1) :
+        for j in range (i) :
+            if (array[j] > array[j+1]) :
+                array[j], array[j+1] = array[j+1], array[j]
+
+
+    
+if (__name__ == "__main__") : 
+    array = list(map(str, input("Enter items separated by spaces \n").split()))
+
+    bubble_sort(array)
+
+    print(f"Sorted List")
+    print(*array)
