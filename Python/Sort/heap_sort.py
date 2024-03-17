b0ce38645ca2d638a992426dcf007a5ba3285ea3
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-03-17 21:39:06 +0900

    Update Heap_Sort.py

--- Python/Sort/heap_sort.py
+++ Python/Sort/heap_sort.py
@@ -1,10 +1,22 @@
-def heapify (array: list) -> list :
+def heapify (array: list, index: int, size: int) -> list :
+    largest = index
+    left = 2 * index + 1
+    right = 2 * index + 2
+
+    if ((size > left) and (array[left] > array[largest])) : largest = left
+    if ((size > right) and (array[right] > array[largest])) : largest = right
+    if (largest != index) :
+        array[index], array[largest] = array[largest], array[index]
+        heapify(array, largest, size)    
     
 
 def heap_sort (array: list) -> list : 
 
     n = len(array)
-    heapify(array, n) 
+    for index in range (n//2 -1, -1, -1) : heapify(array, index, n)
+    for index in range (n-1, 0, -1) :
+        array[0], array[index] = array[index], array[0]
+        heapify(array, 0, index)
 
     
 if (__name__ == "__main__") : 
