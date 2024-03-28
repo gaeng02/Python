2dd3ce71a3e40a482bc59574b86d32706f2ead20
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-03-28 17:42:58 +0900

    Create Performance_Sort.py

---
+++ Python/Sort/Performance.py
@@ -0,0 +1,11 @@
+import "bubble_sort.py"
+import "exchange_sort.py"
+import "heap_sort.py"
+import "insection_sort.py"
+import "merge_sort.py"
+import "quick_sort.py"
+import "selection_sort.py"
+
+random_list = []
+insert_list = []
+reversed_list = []
