a1e7bf29c6c36aea425e6d0651f2fcd26161c928
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-03-30 22:35:16 +0900

    Update Performance_Sort.py

--- Python/Sort/Performance.py
+++ Python/Sort/Performance.py
@@ -9,13 +9,39 @@ import "merge_sort.py"
 import "quick_sort.py"
 import "selection_sort.py"
 
-random_list = []
-insert_list = []
-reversed_list = []
-
 # 1. Test Case Copy
 # 2. Timer Start 
 # 3. Function call
 # 4. Print Result
 # 5. Timer End
 # 6. Append timer result
+
+def Comparison_Sort (test_case: list) -> list : 
+
+  measure_list = []
+  
+  test_list = test_case[:]
+  
+  start = time.time()
+  bubble.sort(test_list)
+  end = time.time()
+
+
+  measure_list.append(end - start)
+
+
+def Check (check_case: list) -> bool :
+  
+  size = 100
+  for i in range (size) : 
+    if (check_case[i] != answer[i]) : 
+      return False
+
+  return True
+
+if (__name__ == "__main__") : 
+  
+  random_list = []
+  insert_list = []
+  reversed_list = []
+  check_list = []
