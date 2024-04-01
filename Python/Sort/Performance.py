9cf681bfdaa1868c508874312869d4f1f17be3e6
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-04-01 18:24:42 +0900

    Update Performance_Sort.py

--- Python/Sort/Performance.py
+++ Python/Sort/Performance.py
@@ -1,48 +1,43 @@
-import time
-import random
+import Sort
 
-import "bubble_sort.py"
-import "exchange_sort.py"
-import "heap_sort.py"
-import "insection_sort.py"
-import "merge_sort.py"
-import "quick_sort.py"
-import "selection_sort.py"
-
-# 1. Test Case Copy
-# 2. Timer Start 
-# 3. Function call
-# 4. Print Result
-# 5. Timer End
-# 6. Append timer result
 
 def Comparison_Sort (test_case: list) -> list : 
 
-  measure_list = []
+    measure_list = []
   
-  test_list = test_case[:]
-  
-  start = time.time()
-  bubble.sort(test_list)
-  end = time.time()
+    test_list = test_case[:]
 
-  if (Check(test_case)) : measure_list.append(false)
-  else : measure_list.append(end - start)
+    Sort.bubble_sort(test_list)
+
+    """
+    if (Check(test_case)) : measure_list.append(false)
+    else : measure_list.append(end - start)
+    """
+
+    return measure_list
 
 
 def Check (check_case: list) -> bool :
-  
-  size = 100
-  for i in range (size) : 
-    if (check_case[i] != answer[i]) : 
-      return False
 
-  return True
+    answer = [1, 2, 3, 4, 5]
+    size = len(answer)
+    
+    for i in range (size) : 
+        if (check_case[i] != answer[i]) :
+            return False
 
-if (__name__ == "__main__") : 
-  
-  random_list = []
-  insert_list = []
-  reversed_list = []
-  duplicated_list = []
-  check_list = []
+    return True
+
+if (__name__ == "__main__") :
+    
+    """
+    random_list = []
+    insert_list = []
+    reversed_list = []
+    duplicated_list = []
+    check_list = []
+    """
+
+    test = [5, 4, 3, 2, 1]
+    print(Comparison_Sort(test))
+    
