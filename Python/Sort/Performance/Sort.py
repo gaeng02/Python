54a550f43907549cf9fc2092becc45c750ae05f6
gaeng_02 <83933215+gaeng02@users.noreply.github.com>
2024-04-11 13:47:32 +0900

    Update Sort_Sort.py

--- Python/Sort/Performance/Sort.py
+++ Python/Sort/Performance/Sort.py
@@ -1,6 +1,6 @@
 # import Sort
 
-def bubble_sort (array: list) -> list : 
+def bubble_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
@@ -14,7 +14,7 @@ def bubble_sort (array: list) -> list :
                 
     return comparison, swap
 
-def exchange_sort (array: list) -> list : 
+def exchange_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
@@ -29,7 +29,7 @@ def exchange_sort (array: list) -> list :
     return comparison, swap
 
 
-def heapify (array: list, index: int, size: int, comparison: int, swap: int) -> list :
+def heapify (array: list, index: int, size: int, comparison: int, swap: int) -> (int, int) :
     largest = index
     left = 2 * index + 1
     right = 2 * index + 2
@@ -50,25 +50,24 @@ def heapify (array: list, index: int, size: int, comparison: int, swap: int) -> 
 
     return comparison, swap
     
-def heap_sort (array: list) -> list : 
+def heap_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
     
     for index in range (n//2 -1, -1, -1) :
-        a, b = heapify(array, index, n, comparison, swap)
-        comparison, swap = comparison + a, swap + b
+        comparison, swap = heapify(array, index, n, comparison, swap)
         
     for index in range (n-1, 0, -1) :
         swap += 1
         array[0], array[index] = array[index], array[0]
-        a, b = heapify(array, 0, index, comparison, swap)
+        a, b = heapify(array, 0, index, 0, 0)
         comparison, swap = comparison + a, swap + b
 
     return comparison, swap
 
 
-def insertion_sort (array: list) -> list : 
+def insertion_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
@@ -84,7 +83,7 @@ def insertion_sort (array: list) -> list :
 
     return comparison, swap
 
-def merge_sort (array: list) -> list : 
+def merge_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
@@ -97,19 +96,23 @@ def merge_sort (array: list) -> list :
 
     a, b = merge_sort(left)
     comparison, swap = comparison + a, swap + b
+    
     a, b = merge_sort(right)
     comparison, swap = comparison + a, swap + b
+    
     comparison, swap = merge(m, n-m, left, right, array, comparison, swap)
 
     return comparison, swap
 
-def merge (l, r, left, right, array,comparison: int, swap: int) :
+def merge (l: int, r: int, left: int, right: int, array: list, comparison: int, swap: int) -> (int, int) :
 
     l, r = l - 1, r - 1
     i, j, k = 0, 0, 0
     
     while ((i <= l) and (j <= r)) :
-        if (left[i] < right[j]) : 
+        comparison += 1
+        swap += 1
+        if (left[i] < right[j]) :
             array[k] = left[i]
             i += 1
         elif (left[i] >= right[j]) :
@@ -126,63 +129,69 @@ def merge (l, r, left, right, array,comparison: int, swap: int) :
         array[k] = right[j]
         j += 1
         k += 1 
-    return 
+    return comparison, swap
 
-def quick_sort (array: list) -> list : 
+def quick_sort (array: list) -> (int, int) : 
 
     n = len(array)
-    _quick_sort(array, 0, n-1) 
+    comparison, swap = 0, 0
+    comparison, swap = _quick_sort_(array, 0, n-1, comparison, swap)
+    return comparison, swap
 
-def _quick_sort (array: list, low: int, high: int) -> list :
- 
+def _quick_sort_ (array: list, low: int, high: int, comparison: int, swap: int) -> (int, int) :
+
     if (low < high) :
-        pivot = partition(array, low, high)
-        _quick_sort(array, low, pivot-1)
-        _quick_sort(array, pivot+1, high)
+        a, b, pivot = partition(array, low, high)
+        comparison, swap = comparison + a, swap + b
+        
+        a, b = _quick_sort_(array, low, pivot-1, 0, 0)
+        comparison, swap = comparison + a, swap + b
+        
+        a, b = _quick_sort_(array, pivot+1, high, 0, 0)
+        comparison, swap = comparison + a, swap + b
 
-def partition (array: list, low: int, high: int) -> int :
-  
+    return comparison, swap
+
+def partition (array: list, low: int, high: int) -> (int, int, int) :
+
+    comparison, swap = 0, 0
     item = array[low]
     j = low
     for i in range (low+1, high+1) :
-        if (array[i] < item) :
+        comparison += 1
+        if (array[i] < item) : 
             j += 1
+            swap += 1
             array[i], array[j] = array[j], array[i]
+    swap += 1
     array[j], array[low] = array[low], array[j]
-    return j
+    return comparison, swap, j
 
 
-def selection_sort (array: list) -> list : 
+def selection_sort (array: list) -> (int, int) : 
 
     n = len(array)
     comparison, swap = 0, 0
     
     for i in range (n) :
         min_index = i
-
+        
         for j in range (i+1, n) :
             comparison += 1
             if (array[min_index] > array[j]) :
                 min_index = j
-                
+
         swap += 1
         array[i], array[min_index] = array[min_index], array[i]
-        
-    return comparison, swap
-    
-if (__name__ == "__main__") :
 
-    '''
-    bubble_sort
-    exchange_sort
-    heap_sort
-    insertion_sort
-    merge_sort
-    quick_sort
-    selection_sort
-    '''
-    array = [5, 4, 3, 2, 1]
-    a, b = heap_sort(array)
-    
-    print(array)
-    print(a, b)
+    return comparison, swap
+
+'''
+bubble_sort
+exchange_sort
+heap_sort
+insertion_sort
+merge_sort
+quick_sort
+selection_sort
+'''
